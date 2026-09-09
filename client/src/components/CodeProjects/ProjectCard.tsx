169a8e142bc895ec224bbdc4dc282f15fa92abf4
crrlyly <128206254+crrlyly@users.noreply.github.com>
2026-09-09 13:59:12 -0400

    Finished some styling for the project cards

--- client/src/components/CodeProjects/ProjectCard.tsx
+++ client/src/components/CodeProjects/ProjectCard.tsx
@@ -1,15 +1,18 @@
 const ProjectCard = () => {
   return (
-    <div className="h-70 w-85 md:h-100 md:w-130 lg:h-150 lg:w-200 shrink-0 border border-pink-200/30 mr-5 flex flex-col p-5 rounded-3xl">
-      <h1 className="text-gray-400 self-end mb-3">1</h1>
-      <span className="text-pink-300 mb-3">
+    <div className="relative group w-85 md:h-100 md:w-130 lg:h-150 lg:w-200 shrink-0 border border-pink-200/30 mr-5 flex flex-col p-5 rounded-3xl">
+      <div className="group-hover:opacity-100 opacity-0 duration-500 transition-opacity z-0 bg-linear-to-b to-[#120817] from-pink-300 absolute top-0 left-0 rounded-3xl w-full h-full bg-yellow-100"></div>
+      <h1 className="z-1 text-black self-end mb-3">1</h1>
+      <span className="z-1 text-pink-300 mb-3">
         2025{" "}
-        <span className="text-purple-200">Web App &middot; Design System</span>
+        <span className="z-1 text-purple-200">
+          Web App &middot; Design System
+        </span>
       </span>
-      <span className="text-white block text-[2.15rem] font-bold mb-3">
+      <span className="z-1 group-hover:translate-x-5 transition-transform duration-300 text-white block text-[2.15rem] font-bold mb-2">
         Lumen Analytics
       </span>
-      <p className="text-white/50">
+      <p className="z-1 text-white/50 opacity-0 group-hover:opacity-100 transition-opacity duration-300">
         A real-time analytics platform with a living, breathing data
         visualization layer.
       </p>
